d71a3293de932738d7d6815562f39559b341c5be
David Henderson <dhend1863@gmail.com>
2019-11-11 12:49:28 -0600

    Everything works minus BandsinTown

--- keys.js
+++ keys.js
@@ -1,2 +1,7 @@
 console.log('this is loaded');
-exports.spotify = { id: process.env.SPOTIFY_ID, secret: process.env.SPOTIFY_SECRET }; 
+
+exports.spotify ={
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+}
+
--- liri.js
+++ liri.js
@@ -1,11 +1,55 @@
-require("dotenv").config();
-let keys = require("./keys.js");
-let Spotify = require('node-spotify-api');
-let request = require('request');
-let omdbAPI = "triology";
-let moment = require("moment");
+require('dotenv').config();
+const keys = require('./keys.js');
+const Spotify = require('node-spotify-api');
+const axios = require('axios')
+const fs = require('fs')
+const moment = require('moment')
 
+const searchType = process.argv[2];
+const searchTerm = process.argv.slice(3).join(' ')
+let txtArr = []
 
+// Switch function for user input
+const searchSwitch = (switchTerm, functionData) => {
+
+    switch (switchTerm) {
+        case 'concert-this':
+            logEntry(switchTerm, functionData);
+            concertThis(functionData);
+            break;
+        case 'spotify-this-song':
+            logEntry(switchTerm, functionData);
+            getSpotify(functionData);
+            break;
+        case 'movie-this':
+            logEntry(switchTerm, functionData);
+            checkMovie(functionData);
+            break;
+        case 'do-what-it-says':
+            doThing();
+            break;
+        default:
+            console.log('IDK What you want dude');
+    }
+}
+// ============================
+
+// Concert Function
+const concertThis = (band) => {
+    let concertURL = `https://rest.bandsintown.com/artists/${band}/events?app_id=codingbootcamp`
+
+    axios.get(concertURL).then(function (response) {
+        let concerts = response.data
+        for (let i = 0; i < concerts.length; i++)
+            console.log(`Venue: ${concerts[i].venue.name}
+Location: ${concerts[i].venue.city}, ${concerts[i].venue.region}
+Date: ${moment(concerts[i].datetime).format('MM/DD/YYYY')}
+_________________________________
+        `);
+    })
+}
+
+// Spotify functions
 var getArtistNames = function(artist){
     return artist.name;
 }
@@ -39,23 +83,60 @@ var getSpotify = function(songName) {
 
 }
 
-request('http://www.omdbapi.com', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-});
+// ==============================
 
-var pick = function(caseData, functionData){
-    switch(caseData){
-        case 'spotify-this-song':
-        getSpotify(functionData);
-        default:
-            console.log('LIRI does not know that one, jabroni')
-    }
+// OMDB Functon 
+const movieThis = (movie) => {
+    let movieURL = `http://www.omdbapi.com/?t=${movie}&y=&plot=short&tomatoes=true&apikey=trilogy`;
+    axios.get(movieURL).then(
+        function (response) {
+            let newMovie = response.data
+            console.log(`
+Title: ${newMovie.Title}
+Year: ${newMovie.Year}
+IMDB Rating: ${newMovie.imdbRating}
+Rotten Tomatoes Rating:${newMovie.Ratings[1].Value}
+Country: ${newMovie.Country}
+Language: ${newMovie.Language}
+Plot: ${newMovie.Plot}
+Actors: ${newMovie.Actors}
+____________________________
+    `);
+        }
+    );
 }
+const checkMovie = (movie) => {
+    if (!movie) {
+        movieThis('Mr. Nobody')
+        console.log(`
+If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
+It's on Netflix!`)
+    } else { movieThis(movie) }
+}
+// ============================
 
-var runThis = function(argOne, argTwo){
-    pick(argOne, argTwo);
-};
+// Do a Thing Function
 
-runThis(process.argv[2], process.argv[3]);
+const doThing = () => {
+    fs.readFile('random.txt', 'utf8', function (err, data) {
+        if (err) {
+            return err
+        }
+        txtArr = data.split(', ');
+        newType = txtArr[0];
+        newSearch = txtArr.slice(1).join(' ')
+        searchSwitch(newType, newSearch);
+    })
+}
+// =============================
+
+// Log function
+const logEntry = (term1, term2) => {
+    let newEntry = `${term1}, ${term2}\n`
+    fs.appendFile('log.txt', newEntry, function (err) {
+        if (err) {
+            throw 'Error'
+        }
+    })
+}
+searchSwitch(searchType, searchTerm)
